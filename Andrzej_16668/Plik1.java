84b248df4aa18369e51331a06a7d1daeae14271f
Andrzej Bończyszyn <baczek1133@gmail.com>
2022-06-19 15:18:44 +0200

    Dodanie plikow z zadania 1 i 2

---
+++ Andrzej_16668/Plik1.java
@@ -0,0 +1,5 @@
+public class Plik1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World edycja 1 edycja 2");
+    }
+}
---
+++ Andrzej_16668/Plik2.java
@@ -0,0 +1,5 @@
+public class Plik2 {
+    public static void main(String[] args) {
+        System.out.println("Zaliczenie CwDW edycja 1 edycja 2");
+    }
+}
